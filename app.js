c4db7dddc5402a697b55d588927cb57fd8f1b918
Arjunyadav910 <arjunyadav667733@gmail.com>
2026-09-06 18:46:12 +0530

    Phase 2(part a)

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.get(
   "/listings/:id",
   wrapAsync(async (req, res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(req.params.id).populate("reviews");
     res.render("listings/show.ejs", { listing });
   }),
 );
@@ -132,11 +132,23 @@ app.post(
     let listing = await Listing.findById(req.params.id);
     let newReview = new Review(req.body.review);
 
-    listing.review.push(newReview);
+    listing.reviews.push(newReview);
 
     await newReview.save();
     await listing.save();
-    res.redirect(`listings/${listing._id}`);
+    res.redirect(`/listings/${listing._id}`);
+  }),
+);
+
+// Delete Review Route
+app.delete(
+  "/listings/:id/reviews/:reviewId",
+  wrapAsync(async (req, res) => {
+    let { id, reviewId } = req.params;
+    await Listing.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+    await Review.findByIdAndDelete(reviewId);
+
+    res.redirect(`/listings/${id}`);
   }),
 );
 
